55dc978dcdb0e2710d2fe8bc03d0c01b2b43e7bb
Indran Jayatharan <indran@calcey.com>
2022-05-31 19:56:17 +0530

    file upload

--- src/app.ts
+++ src/app.ts
@@ -5,6 +5,9 @@ import cors from "cors";
 import db from './models';
 import router from "./routes";
 import {deserializeUser} from "./middleware";
+import { upload } from "./utils/file.util"
+import { Request, Response } from "express";
+import { get } from "lodash";
 
 const port = config.get("port") as number;
 const host = config.get("host") as string;
@@ -16,6 +19,23 @@ app.use(cors())
 app.use(express.json());
 app.use(deserializeUser);
 app.use(router);
+
+app.use('/static', express.static('static'));
+app.use('/postmon', express.static('postmon'));
+
+app.post('/upload-file', upload.single('file'), async (req:Request, res:Response) => {
+    try{
+        const file = get(req, 'file');
+        if(file){
+            res.send(file)
+        }else{
+            return res.status(400).send();
+        }
+    }catch(e){
+        log.error(e);
+        return res.status(400).send(e);
+    }
+})
 
 db.sequelize.sync({
     alter:true
